18b5579da5ae23420fa303d9dafecb7fe8cba2ce
fuhu <fuan.hu@netscout.com>
2022-02-03 11:31:54 +0800

    first commit

---
+++ src/main/java/com/fuhu/djis/DockerJenkinsIntegratonSampleApplication.java
@@ -0,0 +1,13 @@
+package com.fuhu.djis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegratonSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJenkinsIntegratonSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fuhu/djis/DockerJenkinsIntegratonSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuhu.djis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegratonSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
